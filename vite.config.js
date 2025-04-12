f3a1051e91efa33562d9193ea47de562ebe8b235
blokhinamaria <blokhinamariyayu@gmail.com>
2025-04-12 02:22:11 -0400

    Resolved vite.config.js issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sadaveena/',
   plugins: [react()],
-})
+  base: '/Sadaveena/',
+  build: {
+    target: 'esnext', // Use modern JavaScript
+    minify: 'esbuild',
+    cssCodeSplit: true, // Split CSS for better caching
+    sourcemap: false, // Set to true for debugging, false for smaller bundle
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separate vendor dependencies
+          }
+        }
+      }
+    }
+  }
+});
